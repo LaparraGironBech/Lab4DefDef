8cbb15796b498cbf687627d561312d632ecfde45
martinbech <70785044+martinbechfurlan@users.noreply.github.com>
2021-04-25 23:16:28 -0600

    Inicio con singleton

---
+++ Models/Creador.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab4DefDef.Models
+{
+    public class Creador
+    {
+        [Required]
+        public int? Id { get; set; }
+        public string Name { get; set; }
+        public string Lastname { get; set; }
+
+    }
+}
---
+++ Models/Data/Singleton.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab4DefDef.Models.Data
+{
+    public sealed class Singleton
+    {
+        private readonly static Singleton _instance = new Singleton();
+        public List<Creador> CreadorList;
+        private Singleton()
+        {
+            CreadorList = new List<Creador>();
+        }
+
+        public static Singleton Instance
+        {
+            get
+            {
+                return _instance;
+            }
+        }
+    }
+}
